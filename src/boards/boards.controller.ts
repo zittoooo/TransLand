3e9f885d21ea7443bfa4aca9273901e12cb03612
jiholee <stellar7484@naver.com>
2022-07-01 15:16:28 +0900

    Board Model 정의하기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { get } from 'http';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -7,7 +8,7 @@ export class BoardsController {
     constructor(private boardsService: BoardsService) {}
     
     @Get()
-    getAllBoard() {
+    getAllBoard(): Board[] {
         return this.boardsService.getAllBoards();
     };
 }
