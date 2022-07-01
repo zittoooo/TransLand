7163440053aaa1c38c784f084d8d0bffc8c7a338
jiholee <stellar7484@naver.com>
2022-07-01 15:25:24 +0900

    게시물 생성하기(service 부분)

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { randomUUID } from 'crypto';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 
 @Injectable()
 export class BoardsService {
@@ -7,6 +9,18 @@ export class BoardsService {
 
     getAllBoards(): Board[] {
         return this.boards;
+    }
+
+    createBoard(title: string, description: string) {
+        const board: Board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC,
+        }
+
+        this.boards.push(board);
+        return board;
     }
 
 }
