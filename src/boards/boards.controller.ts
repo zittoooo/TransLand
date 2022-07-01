681c73d71d9519507e5e72c6f8519ab700f40810
jiholee <stellar7484@naver.com>
2022-07-01 16:30:44 +0900

    특정 게시물의 상태 업데이트

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { get } from 'http';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
@@ -30,4 +30,13 @@ export class BoardsController {
     deleteBoard(@Param('id') id: string): void {
         this.boardsService.deleteBoard(id);
     }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus,
+    ) {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
+
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -34,4 +34,9 @@ export class BoardsService {
         this.boards = this.boards.filter((board) => board.id !== id);
     }
 
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
+    }
 }
