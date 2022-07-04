9c5dee05d9e01b78d6e087f02c8c4b444a9122da
jiholee <stellar7484@naver.com>
2022-07-04 15:39:47 +0900

    유저 데이터 유효성 체크

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 
@@ -7,7 +7,7 @@ export class AuthController {
     constructor(private authService:AuthService) {}
 
     @Post('/signup')
-    signUp(@Body() authcredentialDto: AuthCredentialDto): Promise<void> {
+    signUp(@Body(ValidationPipe) authcredentialDto: AuthCredentialDto): Promise<void> {
         return this.authService.signUp(authcredentialDto);
     }
 
