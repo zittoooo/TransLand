770f078869c366b95cc5b00601433b09520ffb7d
jiholee <stellar7484@naver.com>
2022-07-05 20:37:12 +0900

    인증된 유저만 게시물 보고 쓸 수 있게 해주기

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { get } from 'http';
 import { BoardStatus } from './board-status.enum';
 import { Board } from './board.entity';
@@ -7,6 +8,7 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
+@UseGuards(AuthGuard())
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
     
